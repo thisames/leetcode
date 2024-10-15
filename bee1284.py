e6a077186ab80415369974c0d9b3802fcc1ca590
thiago <thiiagosantos300@gmail.com>
2024-10-15 05:02:09 -0300

    progress

--- bee1284.py
+++ bee1284.py
@@ -30,3 +30,39 @@ while True:
         print("{0:.2f}".format(round(average + 1e-8, 2)))
     except EOFError:
         break
+
+while True:
+    try:
+        N = int(input())
+        string = [input().strip() for _ in range(N)]
+    except EOFError:
+        break
+
+    real_trie = {}
+
+    for word in string:
+        node = real_trie
+        for char in word:
+            if char not in node:
+                node[char] = {}
+            node = node[char]
+        node['*'] = True
+
+    index = 1
+    build = ""
+    media = 0
+    trie = real_trie
+    for word in string:
+        for char in word:
+            build += char
+            if build == word:
+                break
+            if len(trie[char]) > 1:
+                index += 1
+            trie = trie[char]
+        media = media + index
+        index = 1
+        build = ""
+        trie = real_trie
+
+    print(f"{media / len(string):.2f}")
