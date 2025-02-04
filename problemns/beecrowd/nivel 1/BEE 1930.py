8c3878b64112bf25837d481734f8482abe9e78d2
thisames <thiiagosantos300@gmail.com>
2025-02-04 12:16:04 -0300

    1930 - Tomadas

---
+++ problemns/beecrowd/nivel 1/BEE 1930.py
@@ -0,0 +1,11 @@
+x = input().split(" ")
+
+count = 0
+for i in range(len(x)):
+    if len(x) -1 != i:
+        count = count + (int(x[i]) - 1)
+    else:
+        count = count + int(x[i])
+
+print(count)
+
