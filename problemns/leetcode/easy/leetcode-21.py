aac5a27cef5b5c584423264d74b8f7afa9861a3b
Thiago-Santos-SI <thiiagosantos300@gmail.com>
2025-04-01 19:44:22 -0300

    leetcode-21

--- problemns/leetcode/easy/leetcode-21.py
+++ problemns/leetcode/easy/leetcode-21.py
@@ -1,11 +1,52 @@
-def mergeTwoLists(list1, list2):
-    for i in range(len(list2)):
-        for j in range(len(list1)):
-            if list2[i] <= list1[j]:
-                list1.insert(j, list2[i])
-                break
-
-    return list1
+from typing import Optional
 
 
-print(mergeTwoLists([2, 2, 4, 5], [1, 3, 4]))
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+
+    def add_item(self, head, value):
+        item = ListNode(value)
+
+        if head is None:
+            head = item
+            return head
+
+        current = head
+
+        while current.next:
+            current = current.next
+
+        current.next = item
+        return head
+
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
+        list = []
+
+        newlist = None
+
+        while list1 and list1.next:
+            list.append(list1.val)
+            list1 = list1.next
+
+        if list1:
+            list.append(list1.val)
+
+        while list2 and list2.next:
+            list.append(list2.val)
+            list2 = list2.next
+
+        if list2:
+            list.append(list2.val)
+
+        list = sorted(list)
+
+        for i in list:
+            newlist = self.add_item(newlist, i)
+
+        return newlist
