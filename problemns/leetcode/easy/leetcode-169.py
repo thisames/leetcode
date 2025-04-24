3de794b5af4405b682e1ad732ccef238484c2bc9
thisames <thiiagosantos300@gmail.com>
2025-04-24 02:33:20 -0300

    leetcode-169.py refactoring

--- problemns/leetcode/easy/leetcode-169.py
+++ problemns/leetcode/easy/leetcode-169.py
@@ -2,15 +2,68 @@ from collections import Counter
 from typing import List
 
 
-def majorityElement(nums: List[int]) -> int:
-    num_freq = Counter(nums)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
 
-    number = 0
-    for i in num_freq:
-        if num_freq[i] > len(nums) / 2:
-            number = i
+        num_freq = Counter(nums)
 
-    return number
+        number = 0
+        for i in num_freq:
+            if num_freq[i] > len(nums) / 2:
+                number = i
+
+        return number
 
 
-print(majorityElement([2, 2, 1, 1, 1, 2, 2]))
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        if len(nums) < 2:
+            return nums[0]
+
+        nums.sort()
+
+        i = 0
+        count = 0
+        current_count = 0
+        value = None
+        while i < len(nums) - 1:
+            if i < len(nums) - 1 and nums[i] == nums[i + 1]:
+                while i < len(nums) - 1 and nums[i] == nums[i + 1]:
+                    current_count += 1
+                    i += 1
+                current_count += 1
+                if current_count > count:
+                    count = current_count
+                    value = nums[i]
+                i += 1
+                current_count = 0
+                continue
+            else:
+                current_count += 1
+                if current_count > count:
+                    count = current_count
+                    value = nums[i]
+                i += 1
+                current_count = 0
+
+        return value
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        if len(nums) < 2:
+            return nums[0]
+
+        current_count = 0
+        first = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] != first:
+                current_count -= 1
+            else:
+                current_count += 1
+            if current_count == -1:
+                first = nums[i]
+                current_count = 0
+        return first
